7c1fd9251aab57cd24ec5a0b9230ba36fb7f6615
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-08-28 23:07:47 -0600

    Add header-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineMenu } from "react-icons/md";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <section className="h-screen w-screen bg-primary font-sans">
+        <header className="bg-gradient-to-r from-secondary to-third w-full h-12">
+          <div className="flex items-center h-full mx-6">
+            <div className="flex justify-between w-full">
+              <h1 className="text-pretty text-2xl font-extrabold text-center h-full flex items-center">
+                Jarsdev
+              </h1>
+              <div className="flex items-center">
+                <button className="rounded-xl border-2 border-primary p-0.5">
+                  <MdOutlineMenu className="text-pretty text-3xl" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </header>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#111419",
+        secondary: "#FFCE00",
+        third: "#F79205",
+      },
+    },
   },
   plugins: [],
 }
