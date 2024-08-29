f422064420b09d33c501c2a6c484341af6bb45d6
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-08-28 23:16:21 -0600

    Add profile-design

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,17 @@ function App() {
               <h1 className="text-pretty text-2xl font-extrabold text-center h-full flex items-center">
                 Jarsdev
               </h1>
-              <div className="flex items-center">
+              <div className="flex items-center static">
                 <button className="rounded-xl border-2 border-primary p-0.5">
                   <MdOutlineMenu className="text-pretty text-3xl" />
                 </button>
+                <div className="absolute top-14 right-6 border rounded-lg">
+                  <div className="px-2 py-2">
+                    <h1 className="font-extrabold text-2xl text-white">
+                      Hello World!
+                    </h1>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
