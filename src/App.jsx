114eab9e14dbfe9132dc67d1fdda627535d254c1
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-09-02 23:20:45 -0600

    Add new-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React, {useState} from "react";
-import { MdOutlineMenu } from "react-icons/md";
+import React, { useState } from "react";
+import { MdOutlineMenu, MdOutlineNotifications } from "react-icons/md";
+import { BiMessageDetail } from "react-icons/bi";
 
 function App() {
   const [active, setActive] = useState(false);
   function changeActive() {
     setActive(!active);
     if (active) {
-      document.querySelector('.absolute').style.display = 'none';
-    }
-    else {
-      document.querySelector('.absolute').style.display = 'block';
+      document.querySelector(".absolute").style.display = "none";
+    } else {
+      document.querySelector(".absolute").style.display = "block";
     }
     console.log(active);
-  };
+  }
   return (
     <>
       <section className="h-screen w-screen bg-primary font-sans">
@@ -23,9 +23,16 @@ function App() {
                 Jarsdev
               </h1>
               <div className="flex items-center static">
-                <button onClick={ changeActive } className="rounded-xl border-2 border-primary p-0.5">
-                  <MdOutlineMenu className="text-pretty text-3xl" />
-                </button>
+                <div className="flex space-x-2 items-center">
+                  <BiMessageDetail className="text-pretty text-3xl" />
+                  <MdOutlineNotifications className="text-pretty text-3xl" />
+                  <button
+                    onClick={changeActive}
+                    className="rounded-xl border-2 border-primary p-0.5"
+                  >
+                    <MdOutlineMenu className="text-pretty text-3xl" />
+                  </button>
+                </div>
                 <div className="absolute top-14 right-6 border rounded-lg">
                   <div className="px-2 py-2">
                     <h1 className="font-extrabold text-2xl text-white">
