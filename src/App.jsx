20d1fe0efbbbc865041de8e673f2fd0075854692
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-09-04 23:38:55 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { MdOutlineMenu, MdOutlineNotifications } from "react-icons/md";
-import { BiMessageDetail } from "react-icons/bi";
+import { MdOutlineNotifications } from "react-icons/md";
+import { BiMessageDetail, BiLogOut } from "react-icons/bi";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { PiGearSix } from "react-icons/pi";
+import { TbDeviceAnalytics } from "react-icons/tb";
 
 function App() {
   const [active, setActive] = useState(false);
@@ -32,20 +33,78 @@ function App() {
                     onClick={changeActive}
                     className="rounded-full border-2 border-primary"
                   >
-                    <img src="perfil.jpg" alt="" className="w-10 rounded-full" />
+                    <img
+                      src="perfil.jpg"
+                      alt=""
+                      className="w-10 rounded-full"
+                    />
                   </button>
                 </div>
                 <div className="absolute top-14 right-6 border rounded-lg">
                   <div className="px-2 py-2">
                     <div>
-                      <ul className="text-white">
-                        <li className="flex items-center my-2">
-                          <HiOutlineUserCircle className="text-2xl"/>
-                          <span className="ml-2">My Profile</span>
+                      <ul className="text-white border-b">
+                        <li>
+                          <button className="w-full flex items-center my-2">
+                            <HiOutlineUserCircle className="text-2xl" />
+                            <span className="ml-2">My Profile</span>
+                          </button>
                         </li>
                         <li className="flex items-center my-2">
-                          <PiGearSix className="text-2xl"/>
-                          <span className="ml-2">Account Settings</span>
+                          <button className="w-full flex items-center my-2">
+                            <PiGearSix className="text-2xl" />
+                            <span className="ml-2">Account Settings</span>
+                          </button>
+                        </li>
+                      </ul>
+                      <ul className="text-white border-b">
+                        <li>
+                          <button className="w-full flex items-center my-2">
+                            <TbDeviceAnalytics className="text-2xl" />
+                            <span className="ml-2">Device Management</span>
+                          </button>
+                        </li>
+                        <li className="flex items-center my-2">
+                          <button className="w-full flex items-center my-2">
+                            <BiLogOut className="text-2xl" />
+                            <span className="ml-2">Sign Out</span>
+                          </button>
+                        </li>
+                      </ul>
+                      <ul className="text-white">
+                        <li>
+                          <span className="w-full flex items-center my-2">
+                            Switch Account
+                          </span>
+                        </li>
+                        <li>
+                          <button className="w-full h-full flex items-center my-2">
+                            <div className="flex">
+                              <div>
+                                <img
+                                  className="w-10 rounded-full border"
+                                  src="perfil.jpg"
+                                  alt=""
+                                />
+                              </div>
+                              <div className="flex flex-col text-start">
+                                <span>Angel Ramirez</span>
+                                <span>hi.legacyier@gmail.com</span>
+                              </div>
+                              <div className="h-full flex items-center">
+                                <span className="relative flex h-3 w-3">
+                                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75"></span>
+                                  <span className="relative inline-flex rounded-full h-3 w-3 bg-green-600"></span>
+                                </span>
+                              </div>
+                            </div>
+                          </button>
+                        </li>
+                        <li className="flex items-center my-2">
+                          <button className="w-full flex items-center my-2">
+                            <BiLogOut className="text-2xl" />
+                            <span className="ml-2">Sign Out</span>
+                          </button>
                         </li>
                       </ul>
                     </div>
