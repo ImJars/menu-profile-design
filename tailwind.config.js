f47be3a245722f9029d596be60e4cf5a1e58add0
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-09-06 23:46:46 -0600

    Add colors-animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ export default {
         primary: "#111419",
         secondary: "#FFCE00",
         third: "#F79205",
+
+        // Custom colors
+        newPrimary: "#353736",
+        newSecondary: "#1C1E1D",
+        newThird: "#1D1F1E",
+        newFourth: "#828484",
+        newFifth: "#323334",
       },
     },
   },
